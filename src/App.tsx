52ee45002dae0ee9ea57ddd725c1b6417b6a6245
diegomunizdev <diegomunizdev@outlook.com>
2025-02-14 19:42:44 -0300

    main: commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,51 @@
-import React from "react";
+import { useMemo } from "react";
 
 function App() {
-  const socket = new WebSocket("ws://localhost:3002");
-  let mediaRecorder: MediaRecorder;
+  const socket = useMemo(() => new WebSocket("ws://localhost:3002"), []);
 
-  // content.js
-  // Função para iniciar a captura da tela e áudio
-  async function startRecording() {
-    // 1. Captura a tela
-    console.log("INICIANDO GRAVAÇÃO");
+  let mediaRecorder: MediaRecorder;
+  let streamId: string = "";
+
+  socket.onopen = () => {
+    console.log("Conectado ao WebSocket");
+  };
+
+  socket.onerror = (e) => {
+    console.log("ERROR:", e);
+    throw new Error("Websocket error.");
+  };
+
+  async function start() {
     await navigator.mediaDevices
       .getDisplayMedia({ video: true, audio: true })
       .then((stream) => {
-        // 2. Cria o MediaRecorder para gravar a tela em tempo real
+        streamId = stream.id;
         mediaRecorder = new MediaRecorder(stream, { mimeType: "video/webm" });
-
-        socket.onopen = () => {
-          console.log("Conectado ao WebSocket");
-        };
-
-        socket.onerror = (e) => {
-          console.log("ERROR:", e);
-        };
 
         mediaRecorder.ondataavailable = async (event) => {
           if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
             const arrayBuffer = await event.data.arrayBuffer();
             const uint8Array = new Uint8Array(arrayBuffer);
             console.log("enviando os dados");
-            socket.send(JSON.stringify({ event: "message", data: uint8Array }));
+            socket.send(
+              JSON.stringify({
+                event: "message",
+                data: { streamId: stream.id, buffer: uint8Array },
+              })
+            );
           }
         };
 
-        // 5. Inicia a gravação (a cada 100ms)
-        mediaRecorder.start(100);
+        mediaRecorder.start(1000);
       })
       .catch((error) => {
-        console.error("Erro ao capturar a tela:", error);
         throw new Error(error);
       });
   }
 
-  const start = async () => {
-    startRecording();
-  };
-
   const stop = async () => {
     mediaRecorder.stop();
-    socket.send(JSON.stringify({ event: "end" }));
+    socket.send(JSON.stringify({ event: "end", data: { streamId: streamId } }));
     socket.onclose = () => {
       console.log("Desconectado do websocket");
     };
